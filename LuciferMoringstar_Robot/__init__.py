de2c8369e51cee519fd108cf97593d97f5187db8
TENTACION BOTZ <88924165+pp405810@users.noreply.github.com>
2021-11-05 19:56:51 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -15,27 +15,17 @@ Basic Commads
 
 /start :- check am Alive
 
-Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
+Bot Owner only
+ YOU ARE NOT MY FATHER GO AWAY MR OR MISS 
+ MY FATHER SAID DO NOT GO ANYWERE WITHOUT ME 
+ EVERYDAY BUT I LOVE YOU ALSO
 
 """
 
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [MY FATHER](https://t.me/tentacion_botz)
 
 ➥ Language : Python3
-
-➥ Library : Pyrogram Asyncio 1.13.0
-
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
 """
