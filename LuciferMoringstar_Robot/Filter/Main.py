6ab2cc5e2507c9d6da5b35a027d37b183eb48a6c
TENTACION BOTZ <88924165+pp405810@users.noreply.github.com>
2021-11-29 16:01:39 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**👀 Plot : <code>{plot}</code>\n**,
+        mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**,
 Powered By 😎 {message.chat.title}**"
         files = await get_filter_results(query=search)
         if files:
